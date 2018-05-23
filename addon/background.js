3355d0d362eb67a3b60d533390dbe9abc876327b
karabe <karabe000@gmail.com>
2018-05-23 11:54:29 +0900

    add event listener for tab(add, remove, update)

--- addon/background.js
+++ addon/background.js
@@ -8,8 +8,38 @@ class Groups {
     this.save()
   }
 
+  get current() {
+    return this.groups.default
+  }
+
   save() {
     browser.storage.sync.set({groups: this.groups})
+  }
+
+  add(tab) {
+    console.log(this)
+    this.current.push(tab)
+    this.save()
+  }
+
+  remove(tabId) {
+    const index = this.current.findIndex((tab) => {
+      return tab.id === tabId
+    })
+
+    if (index === -1) return
+
+    this.current.splice(index, 1)
+    this.save()
+  }
+
+  update(tab) {
+    const index = this.current.findIndex((ctab) => {
+      return ctab.id === tab.id
+    })
+
+    this.current[index] = tab
+    this.save()
   }
 }
 
@@ -20,3 +50,13 @@ browser.tabs.query({currentWindow: true})
   .then((tabs) => {
     groups.init(tabs)
   })
+
+browser.tabs.onCreated.addListener((tab) => { groups.add(tab) })
+browser.tabs.onAttached.addListener((tab) => { groups.add(tab) })
+
+browser.tabs.onRemoved.addListener((tabId) => { groups.remove(tabId) })
+browser.tabs.onDetached.addListener((tabId) => { groups.remove(tabId) })
+
+browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  groups.update(tab)
+})
