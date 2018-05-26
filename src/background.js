8e4bbc9de68e4f1d82025c6c23e5f6cedbc9f090
karabe <karabe000@gmail.com>
2018-05-26 22:55:35 +0900

    change var names

--- src/background.js
+++ src/background.js
@@ -4,30 +4,28 @@ import Group from './Group'
 (async () => {
   const repo = new GroupRepository(),
         data = await repo.getAll(),
-        addListener = async (tab) => {
+        createdListener = async (tab) => {
           data.add(await repo.hydrate(tab))
           repo.save(data)
         },
-        removeListener = (tabId) => {
+        removedListener = (tabId) => {
           data.remove(tabId)
           repo.save(data)
         },
-        updateListener = async (tabId, changeInfo, tab) => {
+        updatedListener = async (tabId, changeInfo, tab) => {
           data.update(await repo.hydrate(tab))
           repo.save(data)
         }
 
-  browser.tabs.onCreated.addListener(addListener)
-  browser.tabs.onAttached.addListener(addListener)
+  browser.tabs.onCreated.addListener(createdListener)
 
-  browser.tabs.onRemoved.addListener(removeListener)
-  browser.tabs.onDetached.addListener(removeListener)
+  browser.tabs.onRemoved.addListener(removedListener)
 
-  browser.tabs.onUpdated.addListener(updateListener)
+  browser.tabs.onUpdated.addListener(updatedListener)
 
   const methods = {
     async addNewGroup() {
-      browser.tabs.onCreated.removeListener(addListener)
+      browser.tabs.onCreated.removeListener(createdListener)
 
       const tab = await browser.tabs.create({active: false})
       browser.tabs.hide(tab.id)
@@ -35,7 +33,7 @@ import Group from './Group'
       data.addNewGroup(await repo.hydrate(tab))
       repo.save(data)
 
-      browser.tabs.onCreated.addListener(addListener)
+      browser.tabs.onCreated.addListener(createdListener)
     },
     async changeGroup(index) {
       await data.active(index)
